1e2f79834acd307476b0be6e730d4f2988f3e080
pouijkhk <85764300+pouijkhk@users.noreply.github.com>
2021-06-14 14:28:44 +0300

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,6 +1,21 @@
-x1 = int(input("أدخل العدد الأول:"))
-x2 = int(input("أدخل العدد الثاني:"))
-print(x1, " + ", x2, " = ", x1 + x2)
-print(x1, " - ", x2, " = ", x1 - x2)
-print(x1, " × ", x2, " = ", x1 * x2)
-print(x1, " ÷ ", x2, " = ", x1 / x2)
+try:
+	x1 = int(input("أدخل العدد الأول:"))
+	x2 = int(input("أدخل العدد الثاني:"))
+	print("1-جمع")
+	print("2-طرح")
+	print("3-ضرب")
+	print("4-قسمة")
+	x3 = int(input("أختر:"))
+	if x3 == 1:
+		print(x1, " + ", x2, " = ", x1 + x2)
+	elif x3 == 2:
+		print(x1, " - ", x2, " = ", x1 - x2)
+	elif x3 == 3:
+		print(x1, " × ", x2, " = ", x1 * x2)
+	elif x3 == 4:
+		print(x1, " ÷ ", x2, " = ", x1 / x2)
+	else:
+		print(x1 ** x2)
+
+except:
+	print("HA")
